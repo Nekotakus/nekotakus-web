3a486deee745a9c0c9d878728041bd23cda32942
j33h22 <ju33huang22@gmail.com>
2023-08-28 22:46:22 +0800

    添加首页Banner

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
- 首页占位
+  <div align="center" class="banner">
+    <img width=1000 src="/img/banner/Homepage_1stBanner.png" /><br>
+    <img width=1000 src="/img/banner/Homepage_2ndBanner.png" /><br>
+    <img width=1000 src="/img/banner/Homepage_3rdBanner.png" />
   </div>
 </template>
 
@@ -14,4 +16,7 @@
 //   width: 100%;
 //   background: #6666;
 // }
+.banner{
+  padding: 65px;
+}
 </style>
